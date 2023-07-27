21c184a71479b0d13aba94b7f335c0fa1e0f7bce
Voxel <voxelmc@hotmail.com>
2023-07-27 01:09:19 -0700

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   adapter: vercel(),
-  site: "https://okanagan-igem-inventory.vercel.app/",
+  // site: "https://okanagan-igem-inventory.vercel.app/",
 });
