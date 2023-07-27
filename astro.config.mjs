038f44318a4a50130be6bb9193f479481d98a11d
Voxel <voxelmc@hotmail.com>
2023-07-26 23:54:16 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,4 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  vite: {
-        build: {
-            // workaround bug "index" file
-            // @link https://github.com/withastro/astro/issues/3805
-            rollupOptions: {
-                output: {
-                    entryFileNames: "entry.[hash].js",
-                    chunkFileNames: "chunks/chunk.[hash].js",
-                },
-            },
-        },
-    },
 });
