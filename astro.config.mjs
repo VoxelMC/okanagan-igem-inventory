86cdbdad33ab36541406e090c081f4ed838c3f57
Voxel <voxelmc@hotmail.com>
2023-07-26 23:43:48 -0700

    ???

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,17 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  vite: {
+        build: {
+            // workaround bug "index" file
+            // @link https://github.com/withastro/astro/issues/3805
+            rollupOptions: {
+                output: {
+                    entryFileNames: "entry.[hash].js",
+                    chunkFileNames: "chunks/chunk.[hash].js",
+                },
+            },
+        },
+    },
 });
