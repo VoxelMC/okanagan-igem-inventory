3a140abe24f4657f8213a00b8c21f1296ccbe882
Voxel <voxelmc@hotmail.com>
2023-07-27 10:50:04 -0700

    Try

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,4 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  // site: "https://okanagan-igem-inventory.vercel.app/",
-  redirects: {
-    '/login': '/signin'
-  }
 });
