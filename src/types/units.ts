acbd090d43c3350ebfaa8904eb569169613caf2a
Voxel <voxelmc@hotmail.com>
2023-07-30 21:27:15 -0700

    Create units.ts

---
+++ src/types/units.ts
@@ -0,0 +1,22 @@
+export enum MassUnits {
+	PICOGRAMS = "pg",
+	NANOGRAMS = "ng",
+	MICROGRAMS = "μg",
+	MILLIGRAMS = "mg",
+	GRAMS = "g",
+	KILOGRAMS = "kg",
+}
+
+export enum VolumeUnits {
+	MICROLITRES = "μL",
+	MILLILITRES = "mL",
+	CENTILITRES = "dL",
+	LITRES = "L"
+}
+
+export enum OtherUnits {
+	BOTTLES = "bottles",
+	VIALS = "vials",
+	TUBES = "tubes",
+
+}
