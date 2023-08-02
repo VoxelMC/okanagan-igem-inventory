fe729293e1b7d24277fcff21d830877d3daee80e
Voxel <voxelmc@hotmail.com>
2023-08-01 22:38:58 -0700

    Add Prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
-
 import tailwind from "@astrojs/tailwind";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: vercel(),
-	integrations: [tailwind()]
+  output: 'server',
+  adapter: vercel(),
+  integrations: [tailwind(), prefetch()]
 });
