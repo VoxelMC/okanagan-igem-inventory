6d09ab18938b8efe05f55134dd75fa4fb523dd98
Voxel <voxelmc@hotmail.com>
2023-08-08 10:12:03 -0700

    Node for Production

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-
 import prefetch from "@astrojs/prefetch";
+
+import node from "@astrojs/node";
+
+const mode = import.meta.env.MODE;
+
+const adapter = mode.toLowerCase() == "production" ? node({ mode: "standalone" }) : vercel()
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: adapter,
   integrations: [tailwind(), prefetch()]
 });
