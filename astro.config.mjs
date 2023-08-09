05ca2d436c9d569539fee4cc611e4a1f1a1b9653
Voxel <voxelmc@hotmail.com>
2023-08-08 19:51:43 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,9 @@ import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 
-import node from "@astrojs/node";
-
-const mode = import.meta.env.MODE;
-
-const adapter = mode.toLowerCase() == "production" ? node({ mode: "standalone" }) : vercel()
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: adapter,
-  integrations: [tailwind(), prefetch()]
+    output: 'server',
+    adapter: vercel(),
+    integrations: [tailwind(), prefetch()]
 });
