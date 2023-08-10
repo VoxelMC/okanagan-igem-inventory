f164e5c4fce641056fefdce5a984cb8c072dbb06
Voxel <voxelmc@hotmail.com>
2023-08-10 16:04:45 -0700

    Reformat

--- src/pages/api/auth/signout/index.ts
+++ src/pages/api/auth/signout/index.ts
@@ -1,8 +1,8 @@
 import type { APIRoute } from "astro";
 
 export const get: APIRoute = async ({ redirect, cookies }) => {
-  cookies.delete("session_token", {
-    path: "/",
-  });
-  return redirect("/signin");
+    cookies.delete("session_token", {
+        path: "/",
+    });
+    return redirect("/signin");
 };
