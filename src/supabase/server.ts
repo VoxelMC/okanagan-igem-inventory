4d5d39a94716ea2f580e51864dfeb1e845b631fc
Voxel <voxelmc@hotmail.com>
2023-08-10 13:08:52 -0700

    Update server.ts

--- src/supabase/server.ts
+++ src/supabase/server.ts
@@ -1,5 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
-export const supabase = createClient(
+import * as sb from '@supabase/supabase-js';
+
+export const supabase: sb.SupabaseClient<any, "public", any> = sb.createClient(
 	import.meta.env.SUPABASE_URL,
 	import.meta.env.SUPABASE_ANON_KEY
 );
