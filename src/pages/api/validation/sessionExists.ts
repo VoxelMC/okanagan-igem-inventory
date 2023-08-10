e6dd934290067829f995602badbaaf3b2ba7a3bd
SatanshuMishra <satanshumishra@outlook.com>
2023-08-10 16:37:20 -0700

    Dev Build 1.4 | 🐛 Bug Fixes • Added sessionExists script to check if a user is logged in. • Removed changes from add.astro. Added them to register.astro.

--- src/pages/api/validation/sessionExists.ts
+++ src/pages/api/validation/sessionExists.ts
@@ -1,13 +1,10 @@
 import supabase from "../../../supabase/client";
 
-export default async function sessionExists() {
-    // GET SESSION TOKEN FROM COOKIES
-    const sessionToken = Astro.cookies.get("session_token").value;
-
+export default async function sessionExists(sessionToken: string): Promise<boolean> {
     // CHECK IF IT IS AN ACTIVE SESSION TOKEN
     const {
         data: { user },
     } = await supabase.auth.getUser(sessionToken);
 
-    return user ? user : false;
+    return user ? true : false;
 }
