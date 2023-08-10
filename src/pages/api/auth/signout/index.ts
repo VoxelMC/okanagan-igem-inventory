8a720bb6cc2ee57430486ba44c195ea00bf300af
Voxel <voxelmc@hotmail.com>
2023-08-10 14:55:48 -0700

    Fix Signout

--- src/pages/api/auth/signout/index.ts
+++ src/pages/api/auth/signout/index.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from "astro";
 
 export const get: APIRoute = async ({ redirect, cookies }) => {
-  cookies.delete("session", {
+  cookies.delete("session_token", {
     path: "/",
   });
   return redirect("/signin");
