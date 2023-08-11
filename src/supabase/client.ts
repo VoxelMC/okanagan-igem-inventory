52237301b91bcd87baa871f90b8f7dd549282937
Trevor <voxelmc@hotmail.com>
2023-08-11 15:16:32 -0700

    Added Database Utility to Supabase Client

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -1,6 +1,7 @@
 import * as sb from '@supabase/supabase-js';
+import type { Database } from "./database.types";
 
-const supabase: sb.SupabaseClient<any, "public", any> = sb.createClient(
+const supabase: sb.SupabaseClient<any, "public", any> = sb.createClient<Database>(
 	import.meta.env.SUPABASE_URL,
 	import.meta.env.SUPABASE_ANON_KEY,
 	{
