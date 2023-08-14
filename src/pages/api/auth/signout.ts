ac0ad9d04a97c3fac7d2266dc710fa2ba856192e
Trevor <voxelmc@hotmail.com>
2023-08-13 21:41:06 -0700

    Fixed Signout

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -1,8 +1,11 @@
 import type { APIRoute } from "astro";
+import supabase from "../../../supabase/client";
 
 export const get: APIRoute = async ({ redirect, cookies }) => {
     cookies.delete("session_token", {
         path: "/",
     });
+
+    const { error } = await supabase.auth.signOut();
     return redirect("/signin");
 };
