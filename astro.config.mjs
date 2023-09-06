fe1661ae5802329ae33f49da52b04d5b1998bbaf
Trevor <voxelmc@hotmail.com>
2023-09-05 23:00:29 -0700

    🧹🧹🧹✨

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({ functionPerRoute: false }),
   integrations: [
     react({
       include: ['**/react/*']
