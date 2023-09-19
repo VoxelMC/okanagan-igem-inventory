b619099f414bcebd323b1d8c7600f7403d3308f6
Trevor <voxelmc@hotmail.com>
2023-09-19 01:18:57 -0700

    ✏️ | Add the urlify() function

--- src/supabase/database.functions.ts
+++ src/supabase/database.functions.ts
@@ -3,3 +3,12 @@
 export * from "./functions/database.user";
 export * from "./functions/database.domain";
 export * from "./functions/database.institution";
+
+/**
+ * Turns a string into a standardized URL-ready string.
+ * @param s Text to URLify
+ * @returns string 
+ */
+export function urlify(s: string) {
+	return s.toLowerCase().replaceAll(/(%|{|}|\||\\|\^|~|\[|\]|`| )/gm, "-").replaceAll(/(-{2,}|-$})/gm, "-");
+}
